b8fd52fbc9346217110a6047664bbaa83659d67d
Tonmoy Mojumder <74363531+tonmoy998@users.noreply.github.com>
2026-02-10 10:53:05 +0600

    Adding resolution library push

--- main.lua
+++ main.lua
@@ -6,51 +6,48 @@ local Alert = require("ui.components.alert")
 local screen = require("lib.sunscreen")
 local flux = require("ui.flux")
 local App = require("main.main")
+local push = require("lib.push")
 
 -- Virtual width and height (your design resolution)
--- local vw, vh = 720, 1450
 local vw, vh = 720, 1280
+-- local vw, vh = 720, 1280
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
 function love.load()
-	-- Initialize Sunscreen for responsive screen
-	screen:init({
-		gameWidth = vw,
-		gameHeight = vh,
-		mode = "fit",
-	})
-	App.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+	push:setupScreen(vw, vh, windowWidth, windowHeight, { fullscreen = true })
 end
 
 function love.update(dt)
 	flux.update(dt)
 	local mx, my = love.mouse.getPosition()
 	local vmx, vmy = screen:screenToGame(mx, my)
-	App.update(dt)
 	UI.update(dt)
 end
 
 function love.draw()
-	-- Apply Sunscreen scaling
-	screen:apply()
-
 	-- Draw virtual background
 	-- love.graphics.setColor(primary[1], primary[2], primary[3], 0.4)
 	-- love.graphics.rectangle("fill", 0, 0, vw, vh)
 
+	push:start()
 	-- Draw UI
-	App.draw()
+	-- love.graphics.draw(love.graphics.newImage("floor.png"), 0, 0)
 	UI.draw()
+	push:finish()
 end
 
 function love.resize(w, h)
-	screen:onResize(w, h)
+	return push:resize(w, h)
 end
 
 function love.mousepressed(x, y, button_type)
-	local vmx, vmy = screen:screenToGame(x, y)
+	local vmx, vmy = push:toGame(x, y)
 	UI.mousepressed(vmx, vmy, button_type)
 end
 
 function love.mousereleased(x, y, button_type)
-	local vmx, vmy = screen:screenToGame(x, y)
+	-- local vmx, vmy = screen:screenToGame(x, y)
+	--
+	local vmx, vmy = push:toGame(x, y)
 	UI.mousereleased(vmx, vmy, button_type)
 end
